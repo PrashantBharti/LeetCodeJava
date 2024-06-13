c71963879817aa2cb665cf90645accc6cce0554c
Prashant Bharti <bhartipr@amazon.com>
2024-06-13 20:28:21 +0530

    Solution for LC 3

--- src/lc_3_longest_substring_without_repeating_characters/Solution.java
+++ src/lc_3_longest_substring_without_repeating_characters/Solution.java
@@ -2,13 +2,11 @@ package lc_3_longest_substring_without_repeating_characters;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-
 import static java.util.Objects.isNull;
 
 public class Solution {
     public int lengthOfLongestSubstring(final String input) {
-        if (Objects.isNull(input) || input.isEmpty())
+        if (isNull(input) || input.isEmpty())
             return 0;
 
         final Map<Character, Integer> map = new HashMap<>();
@@ -16,22 +14,16 @@ public class Solution {
         map.put(input.charAt(idxStart), idxStart);
 
         int max = 1;
-        int idxEnd = 1;
-        while (idxEnd < input.length()) {
+        for (int idxEnd = 1; idxEnd < input.length(); idxEnd++) {
             char c = input.charAt(idxEnd);
             if (map.containsKey(c)) {
                 int idx = map.get(c);
-                if (idx < idxStart) {
-                    map.remove(c);
-                } else {
-                    idxStart = idx + 1;
-                }
+                map.remove(c);
+                idxStart = Math.max(idxStart, idx + 1);
             }
 
             map.put(c, idxEnd);
-
             max = Math.max(max, idxEnd - idxStart + 1);
-            idxEnd++;
         }
 
         return max;
