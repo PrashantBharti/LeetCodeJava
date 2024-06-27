99e78aa5d6b4f77b489f37c074e5ca3b3df0bee3
Prashant Bharti <persian_indian@yahoo.co.in>
2024-06-27 08:30:15 +0530

    Solution for LC 11

--- src/Application.java
+++ src/Application.java
@@ -1,12 +1,11 @@
-import lc_10_regular_expression_matching.Solution;
+import lc_11_container_with_most_water.Solution;
 
 class Application {
     public static void main(String[] args) {
         final Solution solution = new Solution();
 
-        String s = "aa";
-        String p = "a*";
-        boolean ans = solution.isMatch(s, p);
-        System.out.println(ans);
+        int[] heights = {1,2,3,4};
+        int area = solution.maxArea(heights);
+        System.out.println(area);
     }
 }
